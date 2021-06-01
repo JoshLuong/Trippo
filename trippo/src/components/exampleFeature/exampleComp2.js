cc8e7a67710685318a9dd5be12df06fc234a473e
v0p1b <joshualuong@hotmail.com>
2021-05-31 19:18:46 -0700

    adding basic structure of react app/ redux/ styled-components

---
+++ trippo/src/components/exampleFeature/exampleComp2.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import * as sc from "./exampleComp2.styles";
+
+function ExampleComp2({children, handleClick}) {
+
+    return (
+      <sc.fancytext onClick={() => {handleClick}}>
+          {children}
+      </sc.fancytext>
+    );
+}
+  
+export default ExampleComp2;
+  
---
+++ trippo/src/components/exampleFeature/exampleComp2.styles.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const fancytext = styled.div`
+    text-align: center;
+    font-style: italic;
+`;

ea12fc4d4dd904d97d0b880fedd2acb4547296b4
r-bassi <50385638+r-bassi@users.noreply.github.com>
2021-06-25 17:18:20 -0700

    added search bar to "Home" page

--- trippo/src/components/searchBar/Searchbar.styles.ts
+++ trippo/src/components/searchBar/Searchbar.styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import * as c from "../../colors/colors";
+
 
 export const Container = styled.div`
   border-radius: 7px;
@@ -7,9 +7,4 @@ export const Container = styled.div`
   display: inline-block;
   width: 45%;
   position: relative;
-  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.45);
-
-  @media (max-width: 1200px) {
-    flex: 0 0 100%;
-  }
 `;
--- trippo/src/components/searchBar/Searchbar.tsx
+++ trippo/src/components/searchBar/Searchbar.tsx
@@ -1,23 +1,77 @@
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import InputBase from "@material-ui/core/InputBase";
+import IconButton from "@material-ui/core/IconButton";
+import SearchIcon from "@material-ui/icons/Search";
 import * as sc from "./Searchbar.styles";
-import Search from "./Search";
+import {DARK_BLUE} from "../../colors/colors";
 
-function Searchbar() {
-  return (
-    <div
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: "2px 4px",
+    display: "flex",
+    alignItems: "center",
+    borderRadius: 25,
+    boxShadow: '0 3px 5px 2px rgba(0, 0, 0, .3)',
+    
+  },
+  input: {
+    marginLeft: theme.spacing(1),
+    flex: 1
+  },
+  iconButton: {
+    padding: 10
+  },
+  divider: {
+    height: 28,
+    margin: 4
+  }
+}));
+
+const ColoredLine = () => (
+  <hr
       style={{
-        marginTop: "10em",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <sc.Container>
-      <Search
-          searchValue
-          onChangeHandler
-        />
-      </sc.Container>
-    </div>
+          color: DARK_BLUE,
+          backgroundColor: DARK_BLUE,
+          height: 13,
+          width: "120hh",
+          position:"relative",
+          display:"flex",
+          top:"3.3em"
+        }}
+  />
+);
+
+export default function Searchbar() {
+  const classes = useStyles();
+
+  return (
+    <>
+    <ColoredLine />
+    <sc.Container>
+      <div
+        style={{
+          height: "0.5em",
+          width: "100%",
+        }}
+      >
+      </div>
+    <Paper component="form" className={classes.root}>
+      <InputBase
+        className={classes.input}
+        placeholder="Search..."
+        inputProps={{ "aria-label": "search itineraries" }}
+      />
+      <IconButton
+        type="submit"
+        className={classes.iconButton}
+        aria-label="search"
+      >
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+    </sc.Container>
+    </>
   );
 }
-
-export default Searchbar;

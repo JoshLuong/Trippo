60d6787d3651d91cad10f211aefb4c9fc275c014
v0p1b <joshualuong@hotmail.com>
2021-07-15 18:54:04 -0700

    adding new models

--- backend/src/database/models.ts
+++ backend/src/database/models.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { Schema, model } from 'mongoose';
+import { Schema, model, Mongoose, ObjectId } from 'mongoose';
 
 export interface IActivity {
   location: {
@@ -37,5 +37,51 @@ export const activitySchema = new Schema<IActivity>({
       comments: String,
   })],
 }, { toObject: { versionKey: false } });
+
+export interface IUser {
+  name: string,
+  email: string,
+  password: string,
+}
+
+export const userSchema = new Schema<IUser>({
+  name: String,
+  email: String,
+  password: String,
+}, { toObject: { versionKey: false } });
+
+export interface IItinerary {
+  user_id: ObjectId;
+  name: string;
+  budget?: number;
+  current_cost?: number;
+  contributers: {
+    user_id: string;
+    name: string;
+  }[];
+  comments?: string;
+  tags?: string[];
+  start_date: Date;
+  end_date: Date;
+}
+
+export const itinerarySchema = new Schema<IItinerary>({
+  user_id: Schema.Types.ObjectId,
+  name: String,
+  budget: Number,
+  current_cost: Number,
+  contributers: [new Schema({
+    user_id: Schema.Types.ObjectId,
+    name: String,
+})],
+  comments: String,
+  tags: [String],
+  start_date: Date,
+  end_date: Date,
+}, { toObject: { versionKey: false } });
+
+export const User = model<IUser>('User', userSchema);
+
+export const Itinerary = model<IItinerary>('Itinerary', itinerarySchema);
 
 export const Activity = model<IActivity>('Activity', activitySchema);

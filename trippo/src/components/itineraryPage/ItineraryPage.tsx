b0ffd505a7faf65f0890eb1e5da83e01f0128261
v0p1b <joshualuong@hotmail.com>
2021-07-23 23:02:42 -0700

    adding expense page

--- trippo/src/components/itineraryPage/ItineraryPage.tsx
+++ trippo/src/components/itineraryPage/ItineraryPage.tsx
@@ -67,6 +67,12 @@ function ItineraryPage() {
           handleNewSlotClick={handleNewSlotClick}
         />
         <sc.SideBar style={{ width: "2em" }}>
+          <sc.StyledReceiptIcon>
+            <ReceiptIcon onClick={() => {
+              setShowExpenses(!showExpenses);
+              setShowItinerary(false);
+            }}/>
+          </sc.StyledReceiptIcon>
           <button onClick={handleOpenItinerary}>
             {showItinerary ? (
               <i className="fas fa-chevron-left"></i>
@@ -80,6 +86,11 @@ function ItineraryPage() {
           <sc.Container>
             <Container />
           </sc.Container> 
+        ) : null}
+        {showExpenses ? (
+          <sc.Container>
+            <ExpensePage />
+         </sc.Container>
         ) : null}
         {
           canOpenNewSlot ? <NewSlot handleClose={handleNewSlotClose}/>

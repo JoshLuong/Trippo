d3f42b4db3abc5dd0c2776deec2507d31a3af3d3
v0p1b <joshualuong@hotmail.com>
2021-08-05 20:25:34 -0700

    fix about page; responsive styling

--- trippo/src/components/aboutPage/AboutPage.styles.ts
+++ trippo/src/components/aboutPage/AboutPage.styles.ts
@@ -1,47 +1,43 @@
 import styled from "styled-components";
 import * as c from "../../colors/colors";
+import EmailIcon from "@material-ui/icons/Email";
+
+export const StyledEmailIcon = styled(EmailIcon)``;
 
 export const containerDiv = styled.div`
   display: flex;
-  position: sticky;
+  overflow-y: hidden;
   min-height: 100%;
   background: url(/about.jpg) no-repeat fixed center;
   background-size: cover;
   flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: center;
   align-items: center;
 `;
 
-
 export const leftHeaderDiv = styled.div`
   text-align: left;
-  margin-left: auto;
-  margin-right: auto;
-  width: 40%;
-  padding-bottom: 50px;
-  
- 
-  `;
+  width: 60%;
+  padding: 5em;
+  flex-align: start;
+
+  @media (max-width: 600px) {
+    width: 100%;
+    padding: 2em;
+  }
+`;
 
 export const aboutHeader = styled.h1`
   color: #ffffff;
   left: 0;
-  text-shadow: 2px 0px 7px ${c.DARK_GREY};  
+  text-shadow: 2px 0px 7px ${c.DARK_GREY};
   font-size: 48px;
   font-weight: extra bold;
 
-  @media(max-width: 1130px){
-    padding-top: 80px;
-    margin-bottom: 0;
-    margin-top: 0;
+  @media (max-width: 600px) {
+    padding-top: 70px;
+    font-size: 40px;
+    text-align: center;
   }
-
-  @media(max-width: 600px){
-  padding-top: 80px;
-  font-size: 40px;
-  }
-  
 `;
 
 export const Divider = styled.div`
@@ -50,42 +46,82 @@ export const Divider = styled.div`
 
 export const descHeader = styled.h2`
   color: #ffffff;
-  text-shadow: 1px 0px 6px ${c.DARK_GREY};  
-  font-size: 26px;
+  font-size: 22px;
   font-weight: normal;
 
-  @media(max-width: 1130px){
+  @media (max-width: 1130px) {
     margin-bottom: 0;
     margin-top: 0;
   }
 
-  @media(max-width: 600px){
-  font-size: 22px;
+  @media (max-width: 600px) {
+    font-size: 20px;
+    text-align: center;
+    a {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
   }
   a:link {
     color: white;
   }
-  
-  /* mouse over link */
-  a:hover {
-    color: blue;
-  }
 
+  a {
+    display: flex;
+    align-items: center;
+
+    span {
+      margin-left: 7px;
+    }
+  }
 `;
 
-export const rightHeaderImg = styled.div`
-  width: 400px;
-  height: 700px;
-  background-image:url('./iphone.png');
-  background-repeat: no-repeat;
-  background-size: contain;
-  margin-left: auto;
-  margin-right: auto;
-  @media(max-width: 600px){
+export const RightHeader = styled.div`
+  position: absolute;
+  right: 0;
+  top: 0;
+  margin: 5em;
+  width: 40%;
+  height: 500px;
+  @media (max-width: 600px) {
+    position: relative;
     display: block;
-    margin-left: auto;
-    margin-right: auto;
-    width: 65%;
+    margin: 0;
+    width: 100%;
+    height: 450px;
   }
-  `;
+`;
 
+export const IPhoneImage1 = styled.img`
+  position: absolute;
+  top: 30%;
+  left: 20%;
+
+  @media (max-width: 910px) {
+    width: 200px;
+    left: 15%;
+  }
+
+  @media (max-width: 600px) {
+    top: 0;
+    width: 200px;
+    left: 15%;
+  }
+`;
+
+export const IPhoneImage2 = styled.img`
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  @media (max-width: 910px) {
+    width: 200px;
+    left: 45%;
+  }
+
+  @media (max-width: 600px) {
+    top: 0;
+    width: 200px;
+    left: 45%;
+  }
+`;
--- trippo/src/components/aboutPage/AboutPage.tsx
+++ trippo/src/components/aboutPage/AboutPage.tsx
@@ -1,34 +1,57 @@
-import * as sc from './AboutPage.styles';
+import * as sc from "./AboutPage.styles";
+
 // TODO: link to user documentation
 function AboutPage() {
-    return (
-        <sc.containerDiv>
-            <sc.leftHeaderDiv>
-                <sc.aboutHeader>About Trippo</sc.aboutHeader>
-                <br/>
-                <sc.descHeader>
-                    <span>
-                    Trippo is a collaborative, map-based, suggestive itinerary planner that allows users to create
-                    create customizable itineraries while managing their expenses at each destination. 
-                    </span>
-                    <br/>
-                    <span>
-                    With Trippo you can plan for your favorite destinations, add collaborators, create budgets, and more!
-                    Using multiple documents and spreadsheets is in the past, start using Trippo for your trips today!
-                    </span>
-                    <br/>
-                    <br/>
-                    
-                    <span>
-                    For questions or concerns regarding Trippo, please see the user documentation "here", or contact us at:&nbsp;
-                    <br/>
-                    <a href="mailto: tripposupport@protonmail.com">tripposupport@protonmail.com</a>
-                    </span>
-                </sc.descHeader>
-            </sc.leftHeaderDiv>
-            <sc.rightHeaderImg/>
-        </sc.containerDiv>
-    )
+  return (
+    <sc.containerDiv>
+      <sc.leftHeaderDiv>
+        <sc.aboutHeader>
+          <span>About Trippo</span>
+        </sc.aboutHeader>
+        <sc.RightHeader>
+          <sc.IPhoneImage1
+            width="35%"
+            src="./iphone1.png"
+            alt="iphone mockup"
+          />
+          <sc.IPhoneImage2
+            width="35%"
+            src="./iphone2.png"
+            alt="iphone mockup"
+          />
+        </sc.RightHeader>
+        <sc.descHeader>
+          <span>
+            Trippo is a collaborative, map-based, suggestive itinerary planner
+            that allows users to create create customizable itineraries while
+            managing their expenses at each destination.
+          </span>
+          <br />
+          <br />
+          <span>
+            With Trippo, you can plan for your favorite destinations, add
+            collaborators, create budgets, get Yelp® suggested activities, and
+            more!
+            <br />
+            Say Goodbye to using traditional documents and spreadsheets, and say
+            Hello to Trippo!
+          </span>
+          <br />
+          <br />
+
+          <span>
+            For questions or concerns regarding Trippo, please see the user
+            documentation "here", or contact us.&nbsp;
+            <br />
+            <a href="mailto: tripposupport@protonmail.com">
+              <sc.StyledEmailIcon />
+              <span>tripposupport@protonmail.com</span>
+            </a>
+          </span>
+        </sc.descHeader>
+      </sc.leftHeaderDiv>
+    </sc.containerDiv>
+  );
 }
 
 export default AboutPage;
